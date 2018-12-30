fa3fdba8e181d08532185475b6c42e487ca580ff
Yves <yalain9@gmail.com>
2018-12-30 16:57:24 -0600

    Project post error validation

--- ppmtool/src/main/java/io/yves/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/yves/ppmtool/web/ProjectController.java
@@ -1,14 +1,22 @@
 package io.yves.ppmtool.web;
 
 import io.yves.ppmtool.domain.Project;
+import io.yves.ppmtool.services.MapValidationErrorService;
 import io.yves.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/project")
@@ -17,8 +25,16 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService errorService;
+
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        ResponseEntity<?> errorMap = errorService.mapValidationService(result);
+
+        if(errorMap != null) return errorMap;
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
