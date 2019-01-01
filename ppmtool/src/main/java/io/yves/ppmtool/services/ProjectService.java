4a635dee5ab71b8608a2fa01cfd199c54e2cccca
Yves <yalain9@gmail.com>
2018-12-31 18:13:20 -0600

    client

--- ppmtool/src/main/java/io/yves/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/yves/ppmtool/services/ProjectService.java
@@ -36,4 +36,14 @@ public class ProjectService {
     public Iterable<Project> findAllProjects() {
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if(project == null) {
+            throw new ProjectIdException("Project with ID " + projectId + " doesn't exist");
+        }
+
+        projectRepository.delete(project);
+    }
 }
--- ppmtool/src/main/java/io/yves/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/yves/ppmtool/web/ProjectController.java
@@ -46,4 +46,11 @@ public class ProjectController {
 
     @GetMapping("/all")
     public Iterable<Project> getAllProjects() {return projectService.findAllProjects();}
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+        projectService.deleteProjectByIdentifier(projectId.toUpperCase());
+
+        return new ResponseEntity<String>("Project successfully deleted", HttpStatus.OK);
+    }
 }
