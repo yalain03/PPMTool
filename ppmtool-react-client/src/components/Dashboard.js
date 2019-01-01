991925e100c48f46d8441852e7da875b76632c91
Yves <yalain9@gmail.com>
2019-01-01 01:10:46 -0600

    get projects

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
 import ProjectItem from './Project/ProjectItem';
 import CreateProjectButton from './Project/CreateButton';
+import { connect } from 'react-redux';
+import { getProjects } from '../actions/projectActions';
+import PropTypes from 'prop-types';
 
 class Dashboard extends Component {
-    render() {
+
+    componentDidMount() {
+        this.props.getProjects();
+    }
+
+    render() {        
+        const projects = this.props.projects.projects;
+
         return (
             <div className="projects">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <h1 className="display-4 text-center">Projects</h1>
-                    <br />
-                    <CreateProjectButton />
-                    <br />
-                    <hr />
-                    <ProjectItem />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4 text-center">Projects</h1>
+                            <br />
+                            <CreateProjectButton />
+                            <br />
+                            <hr />
+                            {
+                                projects.map(project => (<ProjectItem key={project.id} project={project} />))
+                            }                            
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
         );
     }
 }
 
-export default Dashboard;
+Dashboard.protoTypes = ({
+    project: PropTypes.object.isRequired,
+    getProjects: PropTypes.func.isRequired
+});
+
+const mapStateToProps = state => ({
+    projects: state.projects
+});
+
+export default connect(mapStateToProps, {getProjects})(Dashboard);
