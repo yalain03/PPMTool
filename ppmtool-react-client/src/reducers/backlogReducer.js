15ef8ab31bdc37934418447346164aff085084a1
Yves <yalain9@gmail.com>
2019-01-03 13:01:29 -0600

    Project task

---
+++ ppmtool-react-client/src/reducers/backlogReducer.js
@@ -0,0 +1,20 @@
+import { GET_BACKLOG, GET_PROJECT_TASK, DELETE_PROJECT_TASK } from '../actions/types';
+
+const initialState = {
+    project_tasks: [],
+    project_task: {}
+};
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case GET_BACKLOG:
+            return { ...state, project_tasks: action.payload };
+        case GET_PROJECT_TASK:
+            return { ...state, project_task: action.payload };
+        case DELETE_PROJECT_TASK:
+            return { ...state, 
+                project_tasks: state.project_tasks.filter(project_task => project_task.projectIdentifer != action.payload.projectIdentifer) };
+        default:
+            return state;
+    }
+}
